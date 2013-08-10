201a2265b6389adb29c8d2217cfc48c85616788b
borjabrisson <borjabrisson@gmail.com>
2013-08-10 18:03:08 +0100

    update more folders

---
+++ configuracion/actions.h
@@ -0,0 +1,14 @@
+#ifndef ACTION_APP_H_
+#define ACTION_APP_H_
+
+#include <map>
+#include <iostream>
+#include <string>
+#include <list>
+
+using namespace std;
+
+// enum Calendar_ACT {ACT_newNote=321, ACT_editNote, ACT_delNote,ACT_addComment,ACT_editComment,ACT_delComment};
+
+
+#endif /* ACTION_APP_H_ */
---
+++ configuracion/configuracion.h
@@ -0,0 +1,54 @@
+#ifndef CONFIG_H_
+#define CONFIG_H_
+
+#include <map>
+#include <iostream>
+#include <string>
+#include <list>
+#include <set>
+#include <sstream>
+
+#include "actions.h"
+
+using namespace std;
+
+
+#define ERROR_OUT 
+// #define DEBUG_OUT
+
+inline void push_Error(string msg){
+	#ifdef ERROR_OUT
+		cout << "ERROR: "<< msg << endl;
+	#endif
+}
+
+inline void push_Debug(string msg){
+	#ifdef DEBUG_OUT
+		cout << "DEBUG: "<<msg << endl;
+	#endif
+}
+
+typedef struct structItemHeaderColumn{
+	string fieldID;
+	string description;
+	structItemHeaderColumn(string field,string desc){fieldID=field;description=desc;}; // Declaración del constructor
+}itemHeaderColumn;
+
+
+#define SGBD_USER_DEFAULT ""
+#define SGBD_PASS_DEFAULT ""
+#define SGBD_HOST_DEFAULT "127.0.0.1"
+#define SGBD_DB_DEFAULT "test"
+
+#define Enable_login 70000
+#define Enable_autoRefresh 20000
+
+#define strToint(str) (atoi(str.c_str()))
+
+#define wxDateToString(date)  (date.FormatISODate()).utf8_str()
+
+#define wxStringToStdString(str)  str.utf8_str()
+
+
+
+#endif /* CONFIG_H_ */
